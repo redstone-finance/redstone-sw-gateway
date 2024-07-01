ddeeee8c920f696b79b63ecad8e993e28df98129
Asia <ziola.jm@gmail.com>
2024-07-01 15:20:50 +0200

    Warpy - joinSeason2 with left join

--- src/gateway/router/routes/warpy/joinSeason2.ts
+++ src/gateway/router/routes/warpy/joinSeason2.ts
@@ -8,31 +8,35 @@ export async function joinSeason2(ctx: Router.RouterContext) {
 
   const benchmark = Benchmark.measure();
   const result: any = await dbSource.raw(
-    `select id, max(joined::int)::boolean as joined, max(timestamp) as timestamp from(
-      with joined_table as (
+    `with joined_table as (
       select interaction_id, interaction
-      from interactions, 
-      jsonb_array_elements(interaction -> 'tags') tags
-      where contract_id = ${contractId} and function = 'addPoints'
-      and tags ->> 'name' = 'Reward-For' and tags ->> 'value' = 'Join-Season-2'
+      from interactions,
+           jsonb_array_elements(interaction -> 'tags') tags
+      where contract_id = '${contractId}' and function = 'addPoints'
+        and tags ->> 'name' = 'Reward-For' and tags ->> 'value' = 'Join-Season-2'
+      ),
+      joined_table_res as (
+          select true as joined
+          from joined_table, jsonb_array_elements(interaction -> 'tags') as tags, jsonb_array_elements((tags ->> 'value')::jsonb -> 'members') as members
+          where tags ->> 'name' = 'Input'
+          and members::jsonb ->> 'id' = '${userId}'
+      ),
+      registration_table as (
+          select '${userId}' as id, sync_timestamp as timestamp
+          from interactions,
+               jsonb_array_elements(interaction -> 'tags') tags
+          where contract_id = '${contractId}' and function = 'registerUser'
+            and tags ->> 'name' = 'Input'
+            and (tags ->> 'value')::jsonb ->> 'id' = '${userId}'
       )
-      select ${userId} as id, true as joined, null as timestamp
-      from joined_table, jsonb_array_elements(interaction -> 'tags') as tags, jsonb_array_elements((tags ->> 'value')::jsonb -> 'members') as members
-      where tags ->> 'name' = 'Input' 
-      and members::jsonb ->> 'id' = ${userId}
-      union ALL
-      select ${userId} as id, null as joined, sync_timestamp as timestamp
-      from interactions, 
-      jsonb_array_elements(interaction -> 'tags') tags
-      where contract_id = ${contractId} and function = 'registerUser'
-      and tags ->> 'name' = 'Input' 
-      and (tags ->> 'value')::jsonb ->> 'id' = ${userId}
-      ) jt
-      group by id;`
+    select * from registration_table left join joined_table_res on true;`
   );
 
+  const { id, joined, timestamp } = result.rows[0];
   ctx.body = {
-    date: result.rows[0]?.sync_timestamp,
+    id,
+    joined,
+    timestamp,
   };
 
   logger.debug(`User's registration date loaded in ${benchmark.elapsed()}`);

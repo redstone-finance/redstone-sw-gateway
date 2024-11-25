21b4b1240839152e3927eab798ddd61e6b9fe1da
Asia <ziola.jm@gmail.com>
2024-11-25 15:30:27 +0100

    Warpy - join-season-3 - amendments

--- src/gateway/router/routes/warpy/joinSeason3.ts
+++ src/gateway/router/routes/warpy/joinSeason3.ts
@@ -4,38 +4,43 @@ import { Benchmark } from 'warp-contracts';
 export async function joinSeason3(ctx: Router.RouterContext) {
   const { logger, dbSource } = ctx;
 
-  const { contractId, userId } = ctx.query;
+  const { contractId, userId, walletAddress } = ctx.query;
 
   const benchmark = Benchmark.measure();
   const result: any = await dbSource.raw(
     `with joined_table as (
       select interaction_id, interaction
       from interactions,
-           jsonb_array_elements(interaction -> 'tags') tags
-      where contract_id = '${contractId}' and function = 'addPoints'
-        and tags ->> 'name' = 'Reward-For' and tags ->> 'value' = 'Join-Season-3'
+      jsonb_array_elements(interaction -> 'tags') tags
+      where contract_id = ? and function = 'addPoints'
+      and tags ->> 'name' = 'Reward-For' and tags ->> 'value' = 'Join-Season-3'
       ),
       joined_table_res as (
           select true as joined
-          from joined_table, jsonb_array_elements(interaction -> 'tags') as tags, jsonb_array_elements((tags ->> 'value')::jsonb -> 'members') as members
+          from joined_table, 
+          cross join jsonb_array_elements(interaction -> 'tags') as tags, 
+          cross join jsonb_array_elements((tags ->> 'value')::jsonb -> 'members') as members
           where tags ->> 'name' = 'Input'
           and members::jsonb ->> 'id' = ?
       ),
       registration_table as (
-          select ? as id, sync_timestamp as timestamp
-          from interactions,
-               jsonb_array_elements(interaction -> 'tags') tags
-          where contract_id = ? and function = 'registerUser'
-            and tags ->> 'name' = 'Input'
-            and (tags ->> 'value')::jsonb ->> 'id' = ?
+          select sync_timestamp 
+          from interactions, 
+          cross join jsonb_array_elements(interaction -> 'tags') tags, 
+          cross join jsonb_array_elements((tags ->> 'value')::jsonb -> 'members') as members 
+          where contract_id = ? 
+          and function in ('addPoints', 'addPointsForAddress', 'addPointsWithCap') 
+          and tags ->> 'name' = 'Input' 
+          and (members ->> 'id' = ? or members ->> 'id' = ?) 
+          and sync_timestamp between 1719871200000 and 1732575600000 limit 1
       )
     select * from registration_table left join joined_table_res on true;`,
-    [contractId, userId, userId, contractId, userId]
+    [contractId, userId, contractId, userId, walletAddress]
   );
 
   const joinSeason3Result = result.rows[0];
   ctx.body = {
-    id: joinSeason3Result?.id,
+    id: userId,
     joined: joinSeason3Result?.joined,
     timestamp: joinSeason3Result?.timestamp,
   };
